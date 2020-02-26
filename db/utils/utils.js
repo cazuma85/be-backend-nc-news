2789431e90246cc0e97e887bf86d512cb056807f
cazuma85 <alexanderhollingsworth2201@gmail.com>
2020-02-26 12:39:39 +0000

    utils makeRefObj done

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,20 +1,41 @@
 exports.formatDates = list => {
-
   if (list[0] == undefined) return [];
-const newlist = [];
-const maping = list.map(listobj =>{
+  const newlist = [];
+  const maping = list.map(listobj => {
+    const newobj = { ...listobj };
 
-const newobj = {...listobj}
+    newlist.push(newobj);
+  });
+  for (let i = 0; i < newlist.length; i++) {
+    newlist[i].currentTime = new Date().getDate();
+  }
 
-newlist.push(newobj)
-
-})
-newlist[0].currentTime = new Date();
-
- console.log(newlist)
-return newlist;
+  console.log(newlist);
+  return newlist;
+};
+exports.makeRefObj = list => {
+  const newlist = [...list];
+  for (let i = 0; i < newlist.length; i++) {
+    newlist[i] = { ...newlist[i] };
+  }
+  const newnewlist = [];
+  for (let b = 0; b < newlist.length; b++) {
+    if (newlist[b].title) {
+      if (newlist[b].article_id) {
+        const titles = newlist[b].title;
+        const articles = newlist[b].article_id;
+        let object = {};
+        object[titles] = articles;
+        newnewlist.push(object);
+      }
+    }
+  }
+  if (newnewlist.length !== 0) {
+    return newnewlist;
+  }
+  
+  return newlist;
+};
+exports.formatComments = (comments, articleRef) => {
 
 };
-exports.makeRefObj = list => {};
-
-exports.formatComments = (comments, articleRef) => {};
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -20,19 +20,70 @@ describe('formatDates', () => {
       }
     ];
     const actual = formatDates(list);
-    const expected =[
+    const expected = [
       {
         description: 'The man, the Mitch, the legend',
         slug: 'mitch',
-        currentTime: new Date()
+        currentTime: new Date().getDate()
       }
-    ]
+    ];
     expect(actual[0]).to.eql(expected[0]);
+  });
+  it('when given an array with many objects return all objects with a time stamp ', () => {
+    const list = [
+      {
+        description: 'The man, the Mitch, the legend',
+        slug: 'mitch'
+      },
+      {
+        description: 'Not dogs',
+        slug: 'cats'
+      }
+    ];
+    const actual = formatDates(list);
+    const expected = [
+      {
+        description: 'The man, the Mitch, the legend',
+        slug: 'mitch',
+        currentTime: new Date().getDate()
+      },
+      {
+        description: 'Not dogs',
+        slug: 'cats',
+        currentTime: new Date().getDate()
+      }
+    ];
+    expect(actual).to.eql(expected);
   });
 });
 
-describe('makeRefObj', () => {});
-it('', () => {
+describe('makeRefObj', () => {
+  it('if given an array with an object return the array that is not the same as the one given and the object has to be difrent to ', () => {
+    const list = [{}];
+    const actual = makeRefObj(list);
+
+    expect(actual).to.not.equal(list);
+    expect(actual[0]).to.not.equal(list[0]);
+  });
+  it('change the structur of the object to make it so we just have an object with the values of the keys article_id and title ', () => {
+    const list = [{ article_id: 1, title: 'A' }];
+    const actual = makeRefObj(list);
+    const expected = [{ A: 1 }];
+    expect(actual).to.eql(expected);
+  });
+  it('restructure many objects in an array ', () => {
+    const list = [
+      { article_id: 1, title: 'A' },
+      { article_id: 3, title: 'fg' }
+    ];
+    const actual = makeRefObj(list);
+    const expected = [{ A: 1 }, { fg: 3 }];
+    expect(actual).to.eql(expected);
+  });
+});
+
+describe.only('formatComments', () => {
+it('return a new array', () => {
   
 });
-describe('formatComments', () => {});
+});
